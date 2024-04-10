d0cc850d78b3decc52e485073ae2ba5ccf966476
hyungupark <hyungu.1215.park@gmail.com>
2024-04-11 00:13:22 +0900

    modify project

---
+++ src/main/kotlin/com/hyungu/whoseturn/entities/MemberEntity.kt
@@ -0,0 +1,18 @@
+package com.hyungu.whoseturn.entities
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import lombok.Getter
+import lombok.Setter
+
+@Entity(name = "Member")
+@Getter
+@Setter
+class MemberEntity(
+    @Id
+    @GeneratedValue
+    var id: Long? = null,
+    var name: String
+)
+
--- src/main/kotlin/com/hyungu/whoseturn/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hyungu.whoseturn.repository;
-
-import com.hyungu.whoseturn.model.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserModel, Long> {
-}
