48d5bc639769fa0193635d15c0e332475bbc2304
hyungupark <hyungu.1215.park@gmail.com>
2024-04-18 00:40:13 +0900

    modify "RoomController"

--- src/main/kotlin/com/hyungu/whoseturn/controllers/RoomController.kt
+++ src/main/kotlin/com/hyungu/whoseturn/controllers/RoomController.kt
@@ -4,7 +4,10 @@ import com.hyungu.whoseturn.entities.RoomEntity
 import com.hyungu.whoseturn.repositories.RoomRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.Optional
 
 @RestController(value = "rooms")
 class RoomController(val roomRepository: RoomRepository) {
@@ -13,6 +16,9 @@ class RoomController(val roomRepository: RoomRepository) {
     fun findAllRooms(): Iterable<RoomEntity> = roomRepository.findAll()
 
     @GetMapping("/{id}")
-    fun findRoomById(@PathVariable(name = "id") id: String) = roomRepository.findById(id)
+    fun findRoomById(@PathVariable(name = "id") id: String): Optional<RoomEntity> = roomRepository.findById(id)
+
+    @PostMapping
+    fun postRoom(@RequestBody room: RoomEntity): RoomEntity = roomRepository.save(room);
 
 }
