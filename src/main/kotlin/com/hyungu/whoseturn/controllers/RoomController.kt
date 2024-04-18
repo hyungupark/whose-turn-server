aa0fa8e7ae2953821350f36fbe0e9f326fba36f5
hyungupark <hyungu.1215.park@gmail.com>
2024-04-19 00:44:15 +0900

    add "RoomUserEntity"

--- src/main/kotlin/com/hyungu/whoseturn/controllers/RoomController.kt
+++ src/main/kotlin/com/hyungu/whoseturn/controllers/RoomController.kt
@@ -2,23 +2,22 @@ package com.hyungu.whoseturn.controllers
 
 import com.hyungu.whoseturn.entities.RoomEntity
 import com.hyungu.whoseturn.repositories.RoomRepository
+import com.hyungu.whoseturn.repositories.UserRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.util.Optional
 
 @RestController(value = "rooms")
-class RoomController(val roomRepository: RoomRepository) {
+class RoomController(
+    private val roomRepository: RoomRepository,
+    private val userRepository: UserRepository
+) {
 
     @GetMapping
     fun findAllRooms(): Iterable<RoomEntity> = roomRepository.findAll()
 
     @GetMapping("/{id}")
     fun findRoomById(@PathVariable(name = "id") id: String): Optional<RoomEntity> = roomRepository.findById(id)
-
-    @PostMapping
-    fun postRoom(@RequestBody room: RoomEntity): RoomEntity = roomRepository.save(room);
 
 }
---
+++ src/main/kotlin/com/hyungu/whoseturn/entities/RoomUserEntity.kt
@@ -0,0 +1,18 @@
+package com.hyungu.whoseturn.entities
+
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.Table
+
+@Table(name = "room_user")
+data class RoomUserEntity(
+    @Id
+    @JoinColumn(name = "id", table = "rooms")
+    val roomId: String,
+
+    @Id
+    @JoinColumn(name = "id", table = "users")
+    val userId: String,
+
+    var name: String,
+)
