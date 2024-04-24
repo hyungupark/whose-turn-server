e8678ff8ebf855e07b355e706fb57fd8889c06de
hyungupark <hyungu.1215.park@gmail.com>
2024-04-24 23:48:11 +0900

    add database config

--- src/main/java/com/hyungu/whoseturn/entities/RoomEntity.java
+++ src/main/java/com/hyungu/whoseturn/entities/RoomEntity.java
@@ -2,9 +2,6 @@ package com.hyungu.whoseturn.entities;
 
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Table(name = "rooms")
 public class RoomEntity {
@@ -13,9 +10,6 @@ public class RoomEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false)
     private String id;
-
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private List<RoomUserEntity> roomUsers = new ArrayList<>();
 
     public RoomEntity() {
     }
