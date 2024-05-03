52cba279f24b803b22c1fff092a4df6f334c9813
hyungupark <hyungu.1215.park@gmail.com>
2024-05-04 03:08:12 +0900

    add services

--- src/main/java/com/hyungu/whoseturn/services/MemberService.java
+++ src/main/java/com/hyungu/whoseturn/services/MemberService.java
@@ -1,13 +1,33 @@
 package com.hyungu.whoseturn.services;
 
+import com.hyungu.whoseturn.entities.Member;
 import com.hyungu.whoseturn.repositories.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MemberService {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    public List<Member> findAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member saveMember(Member member) {
+        return memberRepository.save(member);
+    }
+
+    public Optional<Member> getMemberById(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    public void deleteMember(Long id) {
+        memberRepository.deleteById(id);
+    }
 
 }
--- src/main/java/com/hyungu/whoseturn/services/RoomService.java
+++ src/main/java/com/hyungu/whoseturn/services/RoomService.java
@@ -1,8 +1,12 @@
 package com.hyungu.whoseturn.services;
 
+import com.hyungu.whoseturn.entities.Room;
 import com.hyungu.whoseturn.repositories.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomService {
@@ -10,4 +14,20 @@ public class RoomService {
     @Autowired
     private RoomRepository roomRepository;
 
+    public List<Room> findAllRooms() {
+        return roomRepository.findAll();
+    }
+
+    public Room saveRoom(Room room) {
+        return roomRepository.save(room);
+    }
+
+    public Optional<Room> getRoomById(Long id) {
+        return roomRepository.findById(id);
+    }
+
+    public void deleteRoom(Long id) {
+        roomRepository.deleteById(id);
+    }
+   
 }
