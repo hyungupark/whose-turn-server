faab332de17f3489ad27cd97daffef33c26f0ef7
hyungupark <hyungu.1215.park@gmail.com>
2024-05-11 00:54:14 +0900

    modify "Spring Security" configs

--- src/main/java/com/hyungu/whoseturn/WebSecurityConfig.java
+++ src/main/java/com/hyungu/whoseturn/WebSecurityConfig.java
@@ -1,4 +1,21 @@
 package com.hyungu.whoseturn;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest().permitAll()
+        );
+        return http.build();
+    }
+ 
 }
