3586e4104284ba01e4de3123f47ade32012a9356
hyungupark <hyungu.1215.park@gmail.com>
2024-05-10 23:30:31 +0900

    remove "Spring Security" configs

--- src/main/java/com/hyungu/whoseturn/WebSecurityConfig.java
+++ src/main/java/com/hyungu/whoseturn/WebSecurityConfig.java
@@ -1,46 +1,4 @@
 package com.hyungu.whoseturn;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
 public class WebSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain servletSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                authorizationManagerRequestMatcherRegistry ->
-                        authorizationManagerRequestMatcherRegistry
-                                .anyRequest()
-                                .authenticated()
-        ).formLogin(
-                httpSecurityFormLoginConfigurer ->
-                        httpSecurityFormLoginConfigurer
-                                .usernameParameter("username")
-                                .passwordParameter("password")
-                                .defaultSuccessUrl("/", true)
-        );
-
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user1").password("pass1").build());
-        return manager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
 }
