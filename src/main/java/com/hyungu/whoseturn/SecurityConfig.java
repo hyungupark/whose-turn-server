ad89bfa25adef6987e2d5b8edc409742379d487e
hyungupark <hyungu.1215.park@gmail.com>
2024-05-17 22:19:43 +0900

    modify "SecurityConfig"

--- src/main/java/com/hyungu/whoseturn/SecurityConfig.java
+++ src/main/java/com/hyungu/whoseturn/SecurityConfig.java
@@ -3,17 +3,54 @@ package com.hyungu.whoseturn;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
-                authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest().permitAll()
-        );
+                        (requests) -> requests
+                                .requestMatchers("/", "/home")
+                                .permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(
+                        (form) -> form
+                                .loginPage("/login")
+                                .permitAll()
+                )
+                .logout(
+                        (logout) -> logout.permitAll()
+                );
         return http.build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User.withUsername("user")
+                        .password(passwordEncoder.encode("password"))
+                        .roles("USER")
+                        .build()
+        );
+        manager.createUser(
+                User.withUsername("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .roles("ADMIN")
+                        .build()
+        );
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
