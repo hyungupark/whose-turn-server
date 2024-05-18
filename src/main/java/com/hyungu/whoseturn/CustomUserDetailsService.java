fddb115692330a444a716d6f8e26d0140210970d
hyungupark <hyungu.1215.park@gmail.com>
2024-05-18 22:23:55 +0900

    modify "CustomUserDetailsService"

--- src/main/java/com/hyungu/whoseturn/CustomUserDetailsService.java
+++ src/main/java/com/hyungu/whoseturn/CustomUserDetailsService.java
@@ -1,7 +1,9 @@
 package com.hyungu.whoseturn;
 
 import com.hyungu.whoseturn.entities.Member;
-import com.hyungu.whoseturn.services.MemberService;
+import com.hyungu.whoseturn.repositories.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,16 +12,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final MemberService memberService;
-
-    public CustomUserDetailsService(MemberService memberService) {
-        this.memberService = memberService;
-    }
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
-        Member member = memberService.getMemberById(Long.valueOf(id)).orElseThrow();
-        return new CustomUserDetails(member);
+        Member member = memberRepository.findById(Long.valueOf(id)).orElse(null);
+        if (member == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return User.builder()
+                .username(member.getName())
+                .build();
     }
 
 }
